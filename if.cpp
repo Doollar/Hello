837337158d76161fa7aeca85ff63ee6f22babc83
Doollar <96178621+Doollar@users.noreply.github.com>
2022-03-10 12:41:46 +0800

    if

---
+++ if.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()
+//{
+//	int age = 10;
+//	if (age < 18)
+//		printf("未成年 \n");
+//	else if (age >= 18 && age < 28)
+//		printf("青年 \n");
+//	else
+//		printf("真正成年 \n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 10;
+//	if (age < 18)
+//		printf("未成年 \n");
+//	else
+//	{
+//		if (age >= 18 && age < 28)
+//			printf("青年 \n");
+//		else if (age >= 28 && age < 58)
+//			printf("壮年 \n");
+//		else
+//			printf("老年 \n");
+//	}
+//
+//		return 0;
+//}
+
+//判断一个数是否为奇数
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	
+//	if (num % 2 == 1)
+//		printf("奇数 \n");
+//	else
+//		printf("偶数 \n");
+//
+//	return 0;
+//}
+
