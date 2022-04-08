100ea5764964ccbcb27c251f998e0758112ad841
Doollar <96178621+Doollar@users.noreply.github.com>
2022-04-08 18:11:04 +0800

    3.14

--- 3.14 p2.cpp
+++ 3.14 p2.cpp
@@ -1,22 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int MyPath(int n);
+int MyPath(int, int);
 
 int main()
 {
-	int n;
-	scanf("%d", &n);
-	printf("%d", MyPath(n));
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d", MyPath(a, b));
 	return 0;
 }
 
-int MyPath(int n)
+int MyPath(int a, int b)
 {
-	if (n < 1)
+	if (b - a < 0)
 		return -1;
-	else if (n == 1 || n == 2)
+	else if (b - a == 0 || b - a == 1)
 		return 1;
+	else if (b - a == 2)
+		return 2;
 	else
-		return MyPath(n - 1) + MyPath(n - 2);
+		return MyPath(a, b - 1) + MyPath(a, b - 2);
 }
